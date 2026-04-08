3600a2d0e330a5e28f44cc0abc941e60e623af4c
XrayOpt-Workstation <xrayopt@local.machine>
2026-04-07 17:53:14 -0700

    Refactor installation check script to include Python version and executable path

--- labjack examples/00a_check_installation.py
+++ labjack examples/00a_check_installation.py
@@ -1,16 +1,25 @@
 """
 Simple script to verify the LabJack LJM library installation and version.
-
 """
 
 # %%
 import os
+import sys
 from labjack import ljm
 
-# %%
-print("### labjack python library version: " + ljm.__version__ + " ###")
+# This library
+import labjack_utils as lju
 
+# %%
+
+print(f"\n## Python version: {sys.version}")
+print(f"Python executable: {sys.executable}")
+
+print("\n### labjack python library version: " + ljm.__version__ + " ###")
 # Print the installation path of the LJM library
 print(f"LJM Library Location: {os.path.dirname(ljm.__file__)}")
+
+# Print the installation path of the labjack_utils library
+print(f"\nlabjack_utils Library Location: {os.path.dirname(lju.__file__)}\n")
 
 ljm.closeAll()  # Close any open connections just in case
